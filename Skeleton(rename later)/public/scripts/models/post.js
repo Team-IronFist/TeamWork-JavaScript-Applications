7f0642d1514bd9d1998929ab03be51b970098af2
Alex Nestorov <nestorov.alexander@gmail.com>
2016-09-30 21:17:53 +0300

    add class Post

---
+++ Skeleton(rename later)/public/scripts/models/post.js
@@ -0,0 +1,36 @@
+function createPost()){
+  class Post {
+    constructor(id, authorId, author, title, description) {
+      this._comments = [];
+
+      this._id = id;
+      this._authorId = authorId;
+      this._author = author;
+      this._title = title;
+      this._description = description;
+    }
+
+    get Id(){
+      return this._id;
+    }
+
+    get PostId(){
+      return this._postId;
+    }
+
+    get Description(){
+      return this._description;
+    }
+    set Description(value){
+      this._description = value;
+    }
+
+  }
+  return {
+    getComment: function (id, authorId, author, title, description){
+      return new Post(id, authorId, author, title, description)
+      }
+    }
+}
+
+export {createPost}
