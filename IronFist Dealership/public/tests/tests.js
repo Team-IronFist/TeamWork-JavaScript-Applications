d99dc6e0f4a612c6df951f164c2b4f6c3c8edddc
StoikoNeykov <stoikoneykov@gmail.com>
2016-10-02 16:33:46 +0300

    remove doublicated libs

--- IronFist Dealership/public/tests/tests.js
+++ IronFist Dealership/public/tests/tests.js
@@ -1,3 +1,13 @@
 mocha.setup('bdd');
 
 const {expect, assert} = chai;
+
+describe('test', function () {
+    it('first-test', function () {
+        assert.equal(-1, [1,2,3].indexOf(4));
+    })
+});
+
+
+
+mocha.run();
